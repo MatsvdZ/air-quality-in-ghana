7c1937a6ff417b498e309c4b6677144eea4ce3d1
MatsVDZ <matshaarlem2005@gmail.com>
2026-01-08 20:34:08 +0100

    Add download functionality for NO₂ measurements and locations data

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
+const ExcelJS = require("exceljs");
 
 const mapController = require("./src/controllers/mapController");
 const Measurement = require("./src/models/measurement");
+const Location = require("./src/models/location");
 
 const app = express();
 const PORT = 3000;
@@ -46,6 +48,67 @@ app.get("/map", (req, res) => {
   res.render("map", {
     title: "Map of Air Quality Ghana"
   });
+});
+
+
+// Download Pagina
+app.get("/download", (req, res) => {
+  res.render("download", { title: "Download data" });
+});
+
+app.get("/download/no2.json", async (req, res) => {
+  try {
+    const data = await Measurement.find({}).sort({ start: -1 }).lean();
+
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Disposition", 'attachment; filename="no2-data.json"');
+
+    res.send(JSON.stringify(data, null, 2));
+  } catch (err) {
+    res.status(500).send("Download failed");
+  }
+});
+
+app.get("/download/no2.xlsx", async (req, res) => {
+  const measurements = await Measurement.find({}).sort({ start: -1 }).lean();
+
+  const wb = new ExcelJS.Workbook();
+  const ws = wb.addWorksheet("NO2");
+
+  ws.columns = [
+    { header: "Tube ID", key: "tubeId" },
+    { header: "Location ID", key: "locationId" },
+    { header: "Period", key: "period" },
+    { header: "Start", key: "start" },
+    { header: "End", key: "end" },
+    { header: "NO2", key: "no2" },
+    { header: "Remarks", key: "remarks" },
+  ];
+
+  measurements.forEach(m => ws.addRow({
+    tubeId: m.tubeId,
+    locationId: m.locationId,
+    period: m.period,
+    start: m.start,
+    end: m.end,
+    no2: m.no2,
+    remarks: m.remarks
+  }));
+
+  res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+  res.setHeader("Content-Disposition", 'attachment; filename="no2-data.xlsx"');
+
+  await wb.xlsx.write(res);
+  res.end();
+});
+
+app.get("/download/locations.json", async (req, res) => {
+  const data = await Location.find({}).sort({ locationId: 1 }).lean();
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Disposition", 'attachment; filename="locations.json"');
+
+  res.send(JSON.stringify(data, null, 2));
 });
 
 // API: Locaties ophalen (via Controller)
